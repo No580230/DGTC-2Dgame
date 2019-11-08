c196b930a4f774c9044665ae745cf477350181a9
No580230 <56706653+No580230@users.noreply.github.com>
2019-11-08 12:09:10 +0800

    程序設定_11/8

---
+++ Unity10.18 Project/Assets/Scripts/Play.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    [Header("速度")][Range(0,100f)]//設定範圍大小
+   public float speed = 3.5f;
+    [Header("跳越")][Range(100,2000)]//設定範圍大小
+    public int jump = 300;
+    [Header("是否在地板上")][Tooltip("用來判定角色是否在地板上。")]//提示文字
+    public bool isGround = false;
+    [Header("人物名稱")]
+    public  string _name = "Jack";
+
+}
